5bf5acc9fc3487e4ef404f2e02beedd3f1cfb833
GarrHFullstack <Garrett.hall@ttu.edu>
2022-11-17 14:19:47 -0600

    G

--- src/WorkoutActivity.js
+++ src/WorkoutActivity.js
@@ -1,2 +1,28 @@
 import {useState, useEffect} from "react"
 import {Link, useParams} from "react-router-dom"
+
+
+const WorkoutActivityPost = () => {
+    try{
+        async function getActivity (){
+            fetch('http://fitnesstrac-kr.herokuapp.com/api/activities/9', {
+                method: "PATCH",
+                body: JSON.stringify({
+                        name: 'Running',
+                        description: 'Keep on running, til you drop!'
+                })
+            })
+        }   
+    }catch(error){
+
+    }
+  
+    
+
+    return (
+        <div>
+            WorkoutPost
+        </div>
+    )
+
+}
